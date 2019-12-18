bd0cc4c0a9c8ade347f06f8c7d555fed39acd967
Paul <support@exceldevacademy.com>
2019-12-17 23:32:52 -0500

    Added DbContext and GitIgnore

---
+++ Context/OrdersManagerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using eda.ordermanager.api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eda.ordermanager.api.Context
+{
+    public class OrdersManagerDbContext : DbContext
+    {
+        public OrdersManagerDbContext(DbContextOptions<OrdersManagerDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<CompanyOrder> CompanyOrders { get; set; }
+    }
+}
