1a27bcff2e84013957fb8bb2e18e79f3e817f9ad
Paul <support@exceldevacademy.com>
2019-12-18 20:45:15 -0500

    initial vendor controller
    working get vendors

---
+++ Controllers/VendorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eda.ordermanager.api.Data.Entities;
+using eda.ordermanager.api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eda.ordermanager.api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/vendors")]
+    public class VendorsController : ControllerBase
+    {
+        private readonly IVendorRepository _vendorRepository;
+
+        public VendorsController(IVendorRepository vendorRepository)
+        {
+            _vendorRepository = vendorRepository ??
+                throw new ArgumentNullException(nameof(vendorRepository));
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<Vendor>> GetVendors()
+        {
+            var vendorsFromRepo = _vendorRepository.GetVendors();
+            return Ok(vendorsFromRepo);
+        }
+    }
+}
